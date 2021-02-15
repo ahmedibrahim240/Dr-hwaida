69e26801dc0132feb4865d4984d5b9b1174bad5f
Ahmedibrahim240 <Ahmedibrahim1310@icloud.com>
2021-02-15 09:55:05 +0200

    some foramting

--- lib/screens/paymenthistory/paymentHistory.dart
+++ lib/screens/paymenthistory/paymentHistory.dart
@@ -1,8 +1,7 @@
 import 'package:DrHwaida/constants/constans.dart';
-import 'package:DrHwaida/constants/themes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'components/visaCard.dart';
 
 class PaymentHistory extends StatefulWidget {
   @override
@@ -10,6 +9,11 @@ class PaymentHistory extends StatefulWidget {
 }
 
 class _PaymentHistoryState extends State<PaymentHistory> {
+  String visaName = 'Visa';
+  String visaNumber = '25555555555555555555555555';
+  String date = '2May202';
+  String price = '555';
+  String userName = 'Ahmed Ibrahim';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,107 +22,19 @@ class _PaymentHistoryState extends State<PaymentHistory> {
         shrinkWrap: true,
         primary: true,
         children: [
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 30,
-              ),
-              width: MediaQuery.of(context).size.width,
-              height: 40,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Colors.grey,
-                    Colors.grey[500],
-                  ],
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Add New Card',
-                    style: AppTheme.heading.copyWith(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                  Icon(
-                    Icons.add,
-                    size: 30,
-                  ),
-                ],
-              ),
-            ),
-          ),
+          addCard(context: context, onTap: () {}),
           ListView.builder(
             shrinkWrap: true,
             primary: false,
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
             itemCount: 10,
             itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      border: Border.all(
-                        color: Colors.grey[300],
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Icon(
-                              FontAwesomeIcons.ccVisa,
-                              color: customColor,
-                              size: 40,
-                            ),
-                            Text(
-                              'Visa',
-                              style: AppTheme.heading,
-                            ),
-                            Text(
-                              '2225*************',
-                              style: AppTheme.heading,
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 15),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '2May202',
-                              style: AppTheme.heading,
-                            ),
-                            Text(
-                              '\$555',
-                              style: AppTheme.heading,
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 15),
-                        Text(
-                          'Consultant Title',
-                          style: AppTheme.heading.copyWith(
-                            color: customColor,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                ],
+              return VisaCard(
+                visaName: visaName,
+                visaNumber: visaNumber,
+                date: date,
+                price: price,
+                userName: userName,
               );
             },
           ),
