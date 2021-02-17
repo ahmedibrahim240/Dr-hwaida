b22fae477de5bc69822821d9a18d9c77033b5a48
kahraba <Ahmedibrahim1310@icloud.com>
2021-02-17 16:51:31 +0200

    add cart in home

--- lib/screens/wrapper/home/components/homeAppBer.dart
+++ lib/screens/wrapper/home/components/homeAppBer.dart
@@ -1,7 +1,9 @@
 import 'package:DrHwaida/constants/themes.dart';
+import 'package:DrHwaida/screens/cart/card.dart';
 import 'package:DrHwaida/screens/notifications/notifications.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomHomeAppBer extends StatelessWidget {
   const CustomHomeAppBer({
@@ -35,45 +37,59 @@ class CustomHomeAppBer extends StatelessWidget {
               ),
             ],
           ),
-          GestureDetector(
-            onTap: () {
+          IconButton(
+            icon: Icon(
+              FontAwesomeIcons.cartPlus,
+              color: Colors.white,
+            ),
+            onPressed: () {
               Navigator.of(context).push(
                 MaterialPageRoute(
-                  builder: (_) => Notificatios(),
+                  builder: (_) => Cart(),
                 ),
               );
             },
-            child: Stack(
-              children: [
-                Icon(
-                  Icons.notifications,
-                  size: 25,
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: Container(
-                    height: 15,
-                    width: 15,
-                    // margin: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.red,
-                    ),
-                    child: Center(
-                      child: Text(
-                        '3',
-                        style: AppTheme.subHeading.copyWith(
-                          color: Colors.white,
-                          fontSize: 10,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
           ),
+
+          // GestureDetector(
+          //   onTap: () {
+          //     Navigator.of(context).push(
+          //       MaterialPageRoute(
+          //         builder: (_) => Notificatios(),
+          //       ),
+          //     );
+          //   },
+          //   child: Stack(
+          //     children: [
+          //       Icon(
+          //         Icons.notifications,
+          //         size: 25,
+          //       ),
+          //       Positioned(
+          //         top: 0,
+          //         right: 0,
+          //         child: Container(
+          //           height: 15,
+          //           width: 15,
+          //           // margin: EdgeInsets.all(5),
+          //           decoration: BoxDecoration(
+          //             shape: BoxShape.circle,
+          //             color: Colors.red,
+          //           ),
+          //           child: Center(
+          //             child: Text(
+          //               '3',
+          //               style: AppTheme.subHeading.copyWith(
+          //                 color: Colors.white,
+          //                 fontSize: 10,
+          //               ),
+          //             ),
+          //           ),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
         ],
       ),
     );
