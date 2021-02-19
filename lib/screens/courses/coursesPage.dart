2f9f47489477abca26260b104a659c298fe225ba
kahraba <Ahmedibrahim1310@icloud.com>
2021-02-19 13:16:05 +0200

    add coursesgraidView2 in courses page

--- lib/screens/courses/coursesPage.dart
+++ lib/screens/courses/coursesPage.dart
@@ -19,13 +19,15 @@ class _CoursesPageState extends State<CoursesPage> {
       body: Stack(
         children: [
           Container(
-            height: MediaQuery.of(context).size.height - 50,
+            height: MediaQuery.of(context).size.height - 60,
             child: ListView(
               shrinkWrap: true,
               primary: true,
               children: [
                 consultantAppBer(context),
                 coursesgraidView(),
+                coursesgraidView2(),
+                SizedBox(height: 20),
               ],
             ),
           ),
@@ -91,6 +93,128 @@ class _CoursesPageState extends State<CoursesPage> {
                                         listCourses[index]
                                             .contant
                                             .substring(0, 50),
+                                        style: AppTheme.subHeading.copyWith(
+                                          fontSize: 10,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      listCourses[index].title,
+                                      style: AppTheme.heading.copyWith(
+                                        color: customColor,
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      date,
+                                      style: AppTheme.heading.copyWith(
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      listCourses[index].oldPrice + '\$',
+                                      style: AppTheme.heading.copyWith(
+                                        color: Colors.grey[500],
+                                        fontSize: 8,
+                                        decoration: TextDecoration.lineThrough,
+                                      ),
+                                    ),
+                                    SizedBox(width: 5),
+                                    Text(
+                                      listCourses[index].newPrice + '\$',
+                                      style: AppTheme.heading.copyWith(
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                RatingStar(
+                                  rating: listCourses[index].rating,
+                                  isReadOnly: true,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  coursesgraidView2() {
+    var size = MediaQuery.of(context).size;
+    final double itemHeight = (size.height - 350) / 3;
+    final double itemWidth = size.width / 2;
+    return GridView.count(
+      crossAxisCount: 1,
+      crossAxisSpacing: 1.0,
+      mainAxisSpacing: 1.0,
+      primary: false,
+      childAspectRatio: (itemWidth / itemHeight),
+      shrinkWrap: true,
+      children: List.generate(
+        listCourses.length,
+        (index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+            child: InkWell(
+              onTap: () {},
+              child: Card(
+                elevation: 4,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                        image: AssetImage(listCourses[index].courseImageUrl),
+                        fit: BoxFit.cover,
+                      )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width -
+                                          50,
+                                      // height: 20,
+                                      child: Text(
+                                        listCourses[index].contant,
                                         style: AppTheme.subHeading.copyWith(
                                           fontSize: 10,
                                         ),
