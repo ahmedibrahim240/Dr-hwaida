a8417a4cffcd9930846d152cfaca803ba4ae8335
kahraba <Ahmedibrahim1310@icloud.com>
2021-04-28 01:11:21 +0200

    ConusltantSearch

--- lib/screens/Consultant/conponents/consultantsearch.dart
+++ lib/screens/Consultant/conponents/consultantsearch.dart
@@ -30,7 +30,59 @@ class ConusltantSearch extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return null;
+    return query.isEmpty
+        ? Container()
+        : FutureBuilder(
+            future: ConsultantApi.consulSreachByName(query),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return (snapshot.data == null || snapshot.data.isEmpty)
+                    ? Container(
+                        child: Center(
+                          child: Text(getTranslated(context, "searchFiled")),
+                        ),
+                      )
+                    : ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, i) {
+                          return ListTile(
+                            leading: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                  child: (snapshot.data[i].image == null)
+                                      ? Container(
+                                          child: Icon(
+                                            Icons.image,
+                                            color: Colors.lightBlueAccent,
+                                          ),
+                                        )
+                                      : customCachedNetworkImage(
+                                          context: context,
+                                          url: snapshot.data[i].image,
+                                        )),
+                            ),
+                            title: Text(snapshot.data[i].name),
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (_) => ConsultantPageView(
+                                    consultant: snapshot.data[i],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          );
   }
 
   @override
